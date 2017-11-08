f66e0ebdacf0438d8e2fd4e1735663762cac91e5
Joe Paul <joeirimpan@gmail.com>
2017-11-08 21:01:05 +0530

    Hmm, nice for range

--- strings.go
+++ strings.go
@@ -9,5 +9,8 @@ func main() {
   fmt.Println(s.Join([]string{"joe", "paul"}, " "))
   fmt.Println(len("joe"))
   var split_words= s.Split("joe-paul", "-")
+  for _, word := range split_words {
+    fmt.Println(word)
+  }
   fmt.Println(split_words[0])
 }
